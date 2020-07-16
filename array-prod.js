9e82e0efeccdb18f1b87584f4f0abb90ba206839
hymntaha <tahauygun@gmail.com>
2020-07-15 23:16:53 -0700

    solution

--- array-prod.js
+++ array-prod.js
@@ -1,5 +1,19 @@
-function findProduct(array) {
+function findProduct(arr) {
+  var prod = 1;
+  var prodArray= [];
+  for (let i = 0; i < arr.length; i++) {
+    prodArray[i] = prod;
+    prod = prod * arr[i]
+  }
 
+  prod = 1;
+  for (let i = arr.length - 1; i > -1; i--) {
+    prodArray[i] *= prod;
+    prod *= arr[i]
+  }
+
+  return prodArray
 }
 
-findProduct([1,2,3,4])
+console.log(findProduct([1,2,3,4]))
+// console.log(findProduct([4,6,8,0]))
