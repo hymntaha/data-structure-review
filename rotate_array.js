473f9300fadf0d2ab34d5d6ae1aaf2b7e14549c3
hymntaha <tahauygun@gmail.com>
2020-07-18 14:59:23 -0700

    splice method

--- rotate_array.js
+++ rotate_array.js
@@ -1,8 +1,5 @@
 function rightRotate(arr,n){
-  var right_slice = arr.slice(n+1, arr.length)
-  var left_slice = arr.slice(0,n+1)
-
-  return right_slice.concat(left_slice);
+  return (arr.splice(arr.length - n)).concat(arr.splice(0, arr.length))
 }
 
 console.log(rightRotate([300,-1,3,0],3))
