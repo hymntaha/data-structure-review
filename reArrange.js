987bd954c8653d39b445e13b23accb13855b2027
hymntaha <tahauygun@gmail.com>
2020-07-20 11:46:23 -0700

    solution for reArrange

--- reArrange.js
+++ reArrange.js
@@ -1,18 +1,43 @@
 function reArrange(arr) {
+  let arrangedArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      arrangedArray.push(arr[i]);
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == 0) {
+      arrangedArray.push(arr[i]);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] > 0) {
+      arrangedArray.push(arr[i]);
+    }
+  }
+
+  return arrangedArray;
+
+}
+
+
+// REARRANGING IN PLACE
+function reArrange2(arr) {
   var leftMostPosEle = 0, tmp;
-  for (var i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] < 0) {
       if (i != leftMostPosEle) {
         tmp = arr[i];
         arr[i] = arr[leftMostPosEle];
         arr[leftMostPosEle] = tmp;
       }
-      leftMostPosEle += 1
+      leftMostPosEle += 1;
     }
   }
   return arr;
-}
-console.log(reArrange([10,-1,20,4,5,-9,-6]
-))
 
-// -1, 10, 20, 4, 5, -9, -6
+}
+console.log(reArrange2([10,-1,20,4,5,-9,-6]))
