d2b8d12a47ef6d5cb80c4ff48ff3ce4664ded63f
hymntaha <tahauygun@gmail.com>
2020-07-19 21:08:22 -0700

    solution study

--- array-prod.js
+++ array-prod.js
@@ -1,18 +1,20 @@
 function findProduct(arr) {
-  var temp = 1,
-    product = [];
-  for (var i = 0; i < arr.length ; i++) {
-    product[i]  = temp;
-    temp = temp * arr[i];
+  let product = [];
+  let temp = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    product[i] = temp;
+    temp = temp * arr[i]
   }
+
+  // 1,1,3,12
 
   temp = 1;
-  for (var i = arr.length - 1; i > -1; i--) {
-    product[i] *= temp;
-    temp *= arr[i];
+  for (let i = arr.length - 1; i > -1; i--) {
+    product[i] = temp * product[i];
+    temp = arr[i] * temp;
   }
-
-  return product
+  return product;
 }
 
 console.log(findProduct([1,3,4,5]))
