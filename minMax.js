ec2965001f6a870880d3c3cf9fa94b3dbaf28d3f
hymntaha <tahauygun@gmail.com>
2020-07-20 12:06:13 -0700

    min-max solution

--- minMax.js
+++ minMax.js
@@ -1,13 +1,5 @@
 function maxMin(arr) {
-  var result = []
-  for (var i = 0; i < (Math.floor(arr.length / 2)); i++) {
-    result.push(arr[arr.length - (i + 1)])
-    result.push(arr[i])
-  }
 
-  if (arr.length % 2)
-    result.push(arr[Math.floor(arr.length / 2)])
-  return result
 }
 
 console.log(maxMin([1,2,3,4,5,6,7]))
