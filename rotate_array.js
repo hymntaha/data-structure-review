d1804d2e7cee9f3374bb957886f31151e7a1c675
hymntaha <tahauygun@gmail.com>
2020-07-20 11:01:27 -0700

    solution

--- rotate_array.js
+++ rotate_array.js
@@ -1,5 +1,7 @@
 function rightRotate(arr,n){
+  console.log(arr.splice(arr.length - n))
+  console.log(arr.splice(0, arr.length))
   return (arr.splice(arr.length - n)).concat(arr.splice(0, arr.length))
 }
 
-console.log(rightRotate([300,-1,3,0],3))
+console.log(rightRotate([1,2,3,4,5],3))
