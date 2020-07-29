c0829c47a0f753942d3f3f3304dbd03724150856
hymntaha <tahauygun@gmail.com>
2020-07-29 10:17:08 -0700

    finishing queue

--- queue/queue_linked_list.js
+++ queue/queue_linked_list.js
@@ -12,16 +12,39 @@ class Queue{
     this.length = 0;
   }
   peek(){
-
+    return this.first;
   }
 
   enqueue(value) {
-
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++
+    return this;
   }
 
   dequeue() {
-
+    if (!this.first) {
+      return null;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return this;
   }
 }
 
 const myQueue = new Queue();
+myQueue.peek();
+myQueue.enqueue('Jordan');
+myQueue.enqueue('Pippen');
+myQueue.enqueue('Rodman')
+myQueue.peek();
+myQueue.dequeue();
