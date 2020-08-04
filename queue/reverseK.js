127fc7beb86df3b52af1a1a7c19ae8c25bfc2906
hymntaha <tahauygun@gmail.com>
2020-08-04 15:43:59 -0700

    starting BST

---
+++ queue/reverseK.js
@@ -0,0 +1,4 @@
+function reverseK(queue, key) {
+
+}
+
---
+++ trees/binary_search_tree.js
@@ -0,0 +1,24 @@
+class Node{
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree{
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+
+  }
+
+  lookup(value) {
+
+  }
+}
+
+
+const tree = new BinarySearchTree();
