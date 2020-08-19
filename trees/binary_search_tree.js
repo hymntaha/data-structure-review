da6688e8c639f2dda3d6446ab8e673c032ebcc32
hymntaha <tahauygun@gmail.com>
2020-08-18 21:29:28 -0700

    BST

--- trees/binary_search_tree.js
+++ trees/binary_search_tree.js
@@ -1,84 +1,200 @@
 class Node{
   constructor(value) {
+    this.value = value;
     this.left = null;
     this.right = null;
-    this.value = value;
   }
+
 }
 
-class BinarySearchTree{
-  constructor() {
-    this.root = null;
+class BST {
+  constructor(value) {
+    this.root = new Node(value);
+    this.count = 1;
+  }
+
+  size(){
+    return this.count;
   }
 
   insert(value) {
-    const newNode = new Node(value);
-    if (this.root === null) {
-      this.root = newNode;
-    } else {
-      let currentNode = this.root;
-      while (true) {
-        if (value < currentNode.value) {
-          //left
-          if (!currentNode.left) {
-            currentNode.left = newNode;
-            return this;
-          }
-          currentNode = currentNode.left;
-        } else {
-          // right
-          if (!currentNode.right) {
-            currentNode.right = newNode;
-            return this;
-          }
-          currentNode = currentNode.right;
+    this.count++
+
+    let newNode = new Node(value)
+
+    const searchTree = node => {
+      // if value < node.value, go left
+      if (value < node.value) {
+        // if no left child, append new node
+        if (!node.left) {
+          node.left = newNode
+        }
+        // if left child, look left again
+        else {
+          searchTree(node.left)
+        }
+      }
+      // if value > node.value, go right
+      else if (value > node.value) {
+        // if no right child, append new node
+        if (!node.right) {
+          node.right = newNode
+        }
+        // if right child, look right again
+        else {
+          searchTree(node.right)
         }
       }
     }
+
+    searchTree(this.root)
   }
 
-  lookup(value) {
-    if (!this.root) {
-      return false;
-    }
+  min() {
     let currentNode = this.root;
+
+    //continue traversing left until no more children
+    while (currentNode.left) {
+      currentNode = currentNode.left;
+    }
+
+    return currentNode.value;
+  }
+
+  max() {
+    let currentNode = this.root;
+
+    //continue traversing right until no more children
+    while (currentNode.right) {
+      currentNode = currentNode.right;
+    }
+
+    return currentNode.value;
+  }
+
+  contains(value) {
+    let currentNode = this.root;
+
     while (currentNode) {
+      if (value === currentNode.value) {
+        return true;
+      }
+
       if (value < currentNode.value) {
         currentNode = currentNode.left;
-      } else if (value > currentNode.value) {
-        currentNode = currentNode.right;
-      } else if (currentNode.value === value) {
-        return currentNode;
+      } else {
+        currentNode = currentNode.right
       }
     }
     return false;
   }
 
-  remove(value) {
-    if (!this.root) {
-      return false;
+
+  // depth first search - branch by branch
+
+  // in-order
+  // left, root, right
+  // 2, 3, 12, 15, 28, 36, 39
+
+  dfsInOrder(){
+
+    let result = [];
+
+    const traverse = node =>{
+
+      // if left child exists, go left again
+      if (node.left) traverse(node.left);
+
+      //capture root node value
+      result.push(node.value);
+
+      // if right child exists, go right again
+      if(node.right) traverse(node.right)
+
     }
+
+    return result
+
   }
+
+// pre-order
+  // root, left, right
+  // 15, 3, 2, 12, 36, 28, 39
+  dfsPreOrder(){
+    let result = [];
+
+    const traverse = node =>{
+      //capture root node value
+      result.push(node.value);
+      // if left child exists, go left again
+      if (node.left) traverse(node.left);
+      // if right child exists, go right again
+      if(node.right) traverse(node.right)
+
+    }
+
+    return result
+  }
+
+// post-order
+  // left, right, root
+  // 2, 12, 3, 28, 39, 36, 15
+  dfsPostOrder() {
+    let result = [];
+
+    const traverse = node =>{
+      // if left child exists, go left again
+      if (node.left) traverse(node.left);
+      // if right child exists, go right again
+      if(node.right) traverse(node.right)
+      //capture root node value
+      result.push(node.value);
+
+
+    }
+
+    return result
+  }
+
+  // breadth first search - level by level
+
+  // use a queue!
+  // 15, 3, 36, 2, 12, 28, 39
+  bfs() {
+    let result = [];
+
+    let queue = [];
+
+    queue.push(this.root);
+
+    while (queue.length) {
+      let currentNode = queue.shift();
+
+      result.push(currentNode);
+
+      if (currentNode.left) {
+        queue.push(currentNode.left)
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right)
+      }
+    }
+
+    return result;
+  }
+
+
 }
 
+//      15
+//   3       6
+// 2  12  28    39
+const bst = new BST(15);
+bst.insert(3);
+bst.insert(36);
+bst.insert(2);
+bst.insert(12);
+bst.insert(28);
+bst.insert(39);
 
-const tree = new BinarySearchTree();
-tree.insert(9);
-tree.insert(4);
-tree.insert(6);
-tree.insert(20);
-tree.insert(170);
-tree.insert(15);
-tree.insert(1);
-console.log(tree.lookup(9));
-console.log(tree.lookup(90));
-// JSON.stringify(traverse(tree.root));
-
-
-function traverse(node) {
-  const tree = {value: node.value}
-  tree.left = node.left === null ? null : traverse(node.left);
-  tree.right = node.right === null ? null : traverse(node.right);
-
-  return tree;
-}
+console.log(bst)
