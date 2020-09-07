b4087a81dbc7453f2b47d0580205af5c2978b6e1
hymntaha <tahauygun@gmail.com>
2020-09-07 16:58:47 -0700

    some problems

--- sliding-window/length_of_longest_substring.js
+++ sliding-window/length_of_longest_substring.js
@@ -1,4 +1,32 @@
 const length_of_longest_substring = function(str, k) {
-  // TODO: Write your code here
-  return -1;
+  let windowStart = 0,
+    maxLength = 0,
+    maxRepeatLetterCount = 0,
+    frequencyMap = {};
+
+  // Try to extend the range [windowStart, windowEnd]
+  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    const rightChar = str[windowEnd];
+    if (!(rightChar in frequencyMap)) {
+      frequencyMap[rightChar] = 0;
+    }
+    frequencyMap[rightChar] += 1;
+    maxRepeatLetterCount = Math.max(maxRepeatLetterCount, frequencyMap[rightChar]);
+    console.log(maxRepeatLetterCount)
+    // Current window size is from windowStart to windowEnd, overall we have a letter which is
+    // repeating 'maxRepeatLetterCount' times, this means we can have a window which has one letter
+    // repeating 'maxRepeatLetterCount' times and the remaining letters we should replace.
+    // if the remaining letters are more than 'k', it is the time to shrink the window as we
+    // are not allowed to replace more than 'k' letters
+    if ((windowEnd - windowStart + 1 - maxRepeatLetterCount) > k) {
+      leftChar = str[windowStart];
+      frequencyMap[leftChar] -= 1;
+      windowStart += 1;
+    }
+
+    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+  }
+  return maxLength;
 };
+
+console.log(length_of_longest_substring('aabccbb',2))
