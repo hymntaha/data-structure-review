de2301a6319ccfeae698a6cca4d9115be89cbdac
hymntaha <tahauygun@gmail.com>
2020-09-10 15:55:42 -0700

    solution

--- sliding-window/square.js
+++ sliding-window/square.js
@@ -1,13 +1,36 @@
 const make_squares = function(arr) {
-  squares = []
+  // BRUTE FORCE
+  // squares = []
+  //
+  // for (let i = 0; i < arr.length; i++) {
+  //   squares[i] = arr[i] * arr[i];
+  // }
+  //
+  // squares.sort((a,b) => a-b);
+  //
+  // return squares;
 
-  for (let i = 0; i < arr.length; i++) {
-    squares[i] = arr[i] * arr[i];
+  let n = arr.length;
+  let squares = Array(n).fill(0);
+  let left = 0;
+  let right = n -1;
+  let highestIdx = n -1;
+  while (left <= right) {
+    console.log(left);
+    console.log(right);
+    let leftSquared = arr[left] * arr[left],
+      rightSquared = arr[right] * arr[right];
+
+    if (leftSquared > rightSquared) {
+      squares[highestIdx] = leftSquared;
+      left++;
+    } else {
+      squares[highestIdx] = rightSquared
+      right--;
+    }
+    highestIdx -= 1;
   }
-
-  squares.sort((a,b) => a-b);
-
-  return squares;
+  return squares
 };
 
 console.log(make_squares([-3, -1, 0, 1, 2]))
---
+++ sliding-window/triple_sum_zero.js
@@ -0,0 +1,12 @@
+const search_triplets = function(arr) {
+  triplets = [];
+  arr.sort((a, b) => a - b);
+  for(i)
+
+
+  return triplets;
+};
+
+console.log(search_triplets([-5, 2, -1, -2, 3]));
+
+// [-5,2,3], [-2,-1,3]
