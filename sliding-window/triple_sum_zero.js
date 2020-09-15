061987ae7d13a313d8914e4e2b8c95a47941fae2
hymntaha <tahauygun@gmail.com>
2020-09-15 11:47:10 -0700

    solution for triplet

--- sliding-window/triple_sum_zero.js
+++ sliding-window/triple_sum_zero.js
@@ -1,40 +1,36 @@
 const search_triplets = function(arr) {
-  console.time('someFunction')
-  arr.sort((a, b) => {
-    return a - b;
-  });
 
-  const result = [];
+  arr.sort((a, b) => a - b);
+
+  let result = [];
 
   for (let indexA = 0; indexA < arr.length - 2; indexA++) {
-
     let indexB = indexA + 1;
-    let indexC = arr.length - 1;
+    let indexC = arr.length -1;
 
-    if (indexA > 0 && arr[indexA] === arr[indexA - 1]) continue;
-
-    while (indexB < indexC ) {
-
+    while (indexB < indexC) {
+      if (indexA > 0 && arr[indexA] === arr[indexA - 1]) continue;
       let sum = arr[indexA] + arr[indexB] + arr[indexC];
-
       if (sum < 0) {
         indexB++;
       } else if (sum > 0) {
         indexC--;
       } else {
         result.push([arr[indexA], arr[indexB], arr[indexC]]);
-        while (arr[indexB] === arr[indexB + 1]) indexB++;
-        while (arr[indexC] === arr[indexC - 1]) indexC--
-        indexB++;
+        while(arr[indexC] === arr[indexC-1]) indexC--;
+        while(arr[indexB] === arr[indexB+1]) indexB++;
+
         indexC--;
+        indexB++;
       }
     }
   }
-  console.timeEnd('someFunction')
+
   return result;
 };
 
 console.log(search_triplets([-5, 2, -1, -2, 3]));
 
+//
 // [-5,2,3], [-2,-1,3]
 // 138ms
