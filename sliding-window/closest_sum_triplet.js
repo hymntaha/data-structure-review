00c184abf7457962958ac0c0cba11875e76306fe
hymntaha <tahauygun@gmail.com>
2020-09-17 11:57:56 -0700

    solution for smaller sum

--- sliding-window/closest_sum_triplet.js
+++ sliding-window/closest_sum_triplet.js
@@ -1,4 +1,4 @@
-const triplet_sum_close_to_target = function(nums, target) {
+const triplet_sum_close_to_target = function (nums, target) {
   nums.sort((a, b) => a - b);
   let closest = null;
 
@@ -26,6 +26,39 @@ const triplet_sum_close_to_target = function(nums, target) {
     while(nums[i] === nums[i + 1]) i++;
   }
   return closest;
+
+  // nums.sort((a, b) => a - b);
+  // let closest = null;
+  //
+  // for (let i = 0; i < nums.length; i++) {
+  //   let left = i + 1;
+  //   let right =nums.length-1;
+  //
+  //   while (left < right) {
+  //     let sum = nums[i] + nums[left] + nums[right];
+  //     let diff = Math.abs(target - sum);
+  //     let currDiff = Math.abs(target - closest);
+  //
+  //     if (diff < currDiff || closest == null) {
+  //       closest = sum;
+  //     }
+  //
+  //
+  //     if (sum === target) {
+  //       return sum;
+  //     } else if (sum < target) {
+  //       left++;
+  //     } else {
+  //       right--;
+  //     }
+  //
+  //     while(nums[i] === nums[i+1]) i++
+  //
+  //
+  //
+  //   }
+  // }
+  // return closest;
 };
 
 console.log(triplet_sum_close_to_target([-3, -1, 1, 2], 1))
---
+++ sliding-window/triplet_with_smaller_sum.js
@@ -0,0 +1,23 @@
+const triplet_with_smaller_sum = function(arr, target) {
+  arr.sort((a, b) => a - b);
+  let count = 0;
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      let sum = arr[i] + arr[right] + arr[left];
+      if (sum < target) {
+        count += right - left;
+        left += 1;
+      } else {
+        right -= 1
+      }
+    }
+  }
+  return count;
+};
+
+console.log(triplet_with_smaller_sum( [-1, 4, 2, 1, 3], 5))
+console.log(triplet_with_smaller_sum( [-1, 0, 2, 3], 3))
