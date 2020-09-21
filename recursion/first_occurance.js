15104285d125c7adf3903a09e6e440f80a201287
hymntaha <tahauygun@gmail.com>
2020-09-21 12:16:41 -0700

    first occurance given target

--- recursion/first_occurance.js
+++ recursion/first_occurance.js
@@ -1,6 +1,27 @@
+// ITERATIVE SOLUTION
+// function firstIndex(arr, testVariable, currentIndex) {
+//   while (currentIndex < arr.length) {
+//     if (arr[currentIndex] == testVariable) {
+//       return currentIndex;
+//     }
+//     currentIndex++;
+//   }
+// }
+
+// RECURSIVE METHOD
 function firstIndex(arr, testVariable, currentIndex) {
-  // Write your code here
-  return null;
+  // Base case 1
+  if (arr.length == currentIndex) {
+    return -1;
+  }
+
+  // Base case 2
+  if (arr[currentIndex] == testVariable) {
+    return currentIndex
+  }
+
+  //Recursive case
+  return firstIndex(arr, testVariable, currentIndex + 1);
 }
 
-console.log(firstIndex([9, 8, 1, 8, 1, 7]))
+console.log(firstIndex([9, 8, 1, 8, 1, 7],1,0))
