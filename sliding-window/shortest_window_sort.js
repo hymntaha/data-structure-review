3dc800030415db39e840d7ba6a1f88231c065a95
hymntaha <tahauygun@gmail.com>
2020-10-07 00:03:58 -0700

    solution for unsorted subarray

--- sliding-window/shortest_window_sort.js
+++ sliding-window/shortest_window_sort.js
@@ -1,38 +1,25 @@
 function shortest_window_sort(arr) {
-  let low = 0,
-    high = arr.length - 1;
-  // find the first number out of sorting order from the beginning
-  while ((low < arr.length - 1 && arr[low] <= arr[low + 1])) {
-    low += 1;
+  let n = arr.length;
+
+  let start = -1;
+  let end = -2;
+
+
+  let min = arr[n - 1];
+  let max = arr[0];
+
+  for (let i = 1; i < n; i++) {
+    let j = n - i - 1;
+    console.log(j)
+    max = Math.max(max, arr[i]);
+    if(arr[i] < max) end = i;
+
+    min = Math.min(min, arr[j]);
+    if(arr[j]>min) start = j;
   }
 
-  if (low === arr.length - 1) { // if the array is sorted
-    return 0;
-  }
 
-  // find the first number out of sorting order from the end
-  while (high > 0 && arr[high] >= arr[high - 1]) {
-    high -= 1;
-  }
-
-  // find the maximum and minimum of the subarray
-  let subarrayMax = -Infinity,
-    subarrayMin = Infinity;
-  for (k = low; k < high + 1; k++) {
-    subarrayMax = Math.max(subarrayMax, arr[k]);
-    subarrayMin = Math.min(subarrayMin, arr[k]);
-  }
-
-  // extend the subarray to include any number which is bigger than the minimum of the subarray
-  while (low > 0 && arr[low - 1] > subarrayMin) {
-    low -= 1;
-  }
-  // extend the subarray to include any number which is smaller than the maximum of the subarray
-  while (high < arr.length - 1 && arr[high + 1] < subarrayMax) {
-    high += 1;
-  }
-
-  return high - low + 1;
+  return end - start + 1;
 }
 
-console.log(shortest_window_sort([1, 2, 5, 3, 7, 10, 9, 12]))
+console.log(shortest_window_sort([2,6,4,8,10,9,15]))
