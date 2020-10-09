e4453bc84cfb9a37a71db165558658aca723cabb
hymntaha <tahauygun@gmail.com>
2020-10-09 11:24:02 -0700

    solution

--- cyclic_sort/find_missing_numbers.js
+++ cyclic_sort/find_missing_numbers.js
@@ -3,20 +3,21 @@ function find_missing_numbers(nums) {
   while (i < nums.length) {
     const j = nums[i] - 1;
     if (nums[i] !== nums[j]) {
-      [nums[i], nums[j]] = [nums[j], nums[i]]; // swap
+      [nums[i], nums[j]] = [nums[j], nums[i]]
     } else {
-      i += 1;
-    }
-  }
-  missingNumbers = [];
-
-  for (i = 0; i < nums.length; i++) {
-    if (nums[i] !== i + 1) {
-      missingNumbers.push(i + 1);
+      i +=1;
     }
   }
 
-  return missingNumbers;
+    let missingValues = [];
+
+    for (i = 0; i < nums.length; i++) {
+      if (nums[i] !== i + 1) {
+        missingValues.push(i+1)
+      }
+    }
+
+    return missingValues
 }
 
 console.log(find_missing_numbers([2, 4, 1, 2]))
