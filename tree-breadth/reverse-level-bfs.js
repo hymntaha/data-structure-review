21919e17521f5e0850fb7d9ad7bc32785a9d5cdd
hymntaha <tahauygun@gmail.com>
2020-10-13 14:50:59 -0700

    adding bfs traversal

---
+++ tree-breadth/reverse-level-bfs.js
@@ -0,0 +1,20 @@
+class TreeNode{
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const traverse = function (root) {
+  result = [];
+
+  return result;
+};
+var root = new TreeNode(12)
+root.left = new TreeNode(7)
+root.right = new TreeNode(1)
+root.left.left = new TreeNode(9)
+root.right.left = new TreeNode(10)
+root.right.right = new TreeNode(5)
+console.log(`Reverse level order traversal: ${traverse(root)}`)
--- tree-breadth/tree-traversal.js
+++ tree-breadth/tree-traversal.js
@@ -1,21 +1,42 @@
-class TreeNode{
-  constructor(value) {
-    this.value = value;
+const Deque = require('collections/deque')
+class TreeNode {
+  constructor(val) {
+    this.val = val;
     this.left = null;
     this.right = null;
   }
 }
 
-const traverse = function (root) {
-  console.log(root)
-  result = [];
 
+function traverse(root) {
+  result = [];
   if (root === null) {
     return result;
   }
 
   const queue = new Deque();
-};
+  queue.push(root);
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    currentLevel = [];
+    for (i = 0; i < levelSize; i++) {
+      currentNode = queue.shift();
+      // add the node to the current level
+      currentLevel.push(currentNode.val);
+      // insert the children of current node in the queue
+      if (currentNode.left !== null) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right !== null) {
+        queue.push(currentNode.right);
+      }
+    }
+    result.push(currentLevel);
+  }
+
+  return result;
+}
+
 
 var root = new TreeNode(12);
 root.left = new TreeNode(7);
